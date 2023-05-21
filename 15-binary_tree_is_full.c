eab77953ed1e0172103447bd8d6e8533bac52a85
omda3310 <6197@holbertonstudents.com>
2023-05-21 19:38:02 +0200

    add 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,17 +7,17 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    if (!tree)
-    {
-        return (0);
-    }
-    if (tree->left == NULL && tree->right == NULL)
-    {
-        return (1);
-    }
-    if (tree->left && tree->right)
-    {
-        return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
-    }
-    return (0);
+	if (!tree)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	if (tree->left && tree->right)
+	{
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	}
+	return (0);
 }
