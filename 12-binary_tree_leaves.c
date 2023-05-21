27c3a8435fce9567aa62ebc0e420dae871985622
omda3310 <6197@holbertonstudents.com>
2023-05-21 17:37:22 +0200

    add 12-binary-tree_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - count tree-leaves
+ * tree : binary tree
+ * Return : number of leaves
+ *
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+	{
+		return (0);
+	}
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+	else
+	{
+		return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+	}
+}
