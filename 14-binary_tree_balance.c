1690f550e6da23e568ff79d475be215e5f918db5
omda3310 <6197@holbertonstudents.com>
2023-05-21 19:08:45 +0200

    add 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,8 +7,6 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int height_left = 1, height_right = 1;
-
 	if (tree)
 		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 	return (0);
