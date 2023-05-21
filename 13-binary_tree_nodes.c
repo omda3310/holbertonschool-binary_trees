cc9dc0b76fdcb6f27951125fee64242cf3397fd8
omda3310 <6197@holbertonstudents.com>
2023-05-21 18:21:06 +0200

    add 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -10,24 +10,16 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t count = 0;
 
-	if (tree)
-	{
-		if (tree->left != NULL)
-		{
-			if (tree->right == NULL)
-			{
-				count += 1;
-			}
-		}
-		count += binary_tree_nodes(tree->left);
-		if (tree->right != NULL)
-		{
-			if (tree->left == NULL)
-			{
-				count += 1;
-			}
-		}
-		count += binary_tree_nodes(tree->right);
-		return (count);
-	}
+	if (!tree)
+		return (0);
+
+	if (tree->left != NULL)
+		if (tree->right == NULL)
+			count += 1;
+	count += binary_tree_nodes(tree->left);
+	if (tree->right != NULL)
+		if (tree->left == NULL)
+			count += 1;
+	count += binary_tree_nodes(tree->right);
+	return (count);
 }
