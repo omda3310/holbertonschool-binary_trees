b3f5b9179f13f8a179c622ae1306220e537feae6
omda3310 <6197@holbertonstudents.com>
2023-05-21 16:55:13 +0200

    update function

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,16 +13,18 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-
-	/* Recursively calculate the height of the left subtree */
-	left_height = binary_tree_height(tree->left);
-
-	/* Recursively calculate the height of the right subtree */
-	right_height = binary_tree_height(tree->right);
-
-	/* Return the maximum height between the left and right subtrees */
-	if (left_height > right_height)
-		return (left_height + 1);
 	else
-		return (right_height + 1);
+	{
+		/* Recursively calculate the height of the left subtree */
+		left_height = binary_tree_height(tree->left);
+
+		/* Recursively calculate the height of the right subtree */
+		right_height = binary_tree_height(tree->right);
+
+		/* Return the maximum height between the left and right subtrees */
+		if (left_height > right_height)
+			return (left_height + 1);
+		else
+			return (right_height + 1);
+	}
 }
